885f77b46a0380f3d813e2bbb7d6ff0576bbc5e6
juel <juelcse2019@gmail.com>
2024-12-05 22:47:02 +0600

    design update

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,14 +30,30 @@ jQuery(document).ready(function ($) {
       time: 1500
   });
 
-  $('.nav-bar-icon').click(function(e) {
-    e.stopPropagation(); // Prevent click event from bubbling up
-    $('.navbar-nav').slideToggle();
-  });
-  $(document).click(function(e) {
-    if (!$(e.target).closest('.navbar-nav').length) {
-      $('.navbar-nav').slideUp();
+  $(document).ready(function () {
+    function toggleNavBar() {
+      if (window.innerWidth <= 768) { // Check if screen width is 768px or smaller
+        $('.nav-bar-icon').off('click').on('click', function (e) {
+          e.stopPropagation(); // Prevent click event from bubbling up
+          $('.navbar-nav').slideToggle();
+        });
+  
+        $(document).off('click').on('click', function (e) {
+          if (!$(e.target).closest('.navbar-nav, .nav-bar-icon').length) {
+            $('.navbar-nav').slideUp();
+          }
+        });
+      } else {
+        // Reset styles and events for larger screens
+        $('.navbar-nav').removeAttr('style'); // Removes inline styles like display:none
+        $('.nav-bar-icon').off('click');
+        $(document).off('click');
+      }
     }
+  
+    // Call function on page load and on window resize
+    toggleNavBar();
+    $(window).resize(toggleNavBar);
   });
-
+  
 })
